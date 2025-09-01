49e1a08b7e94a9a7c174b139308de8035064c27d
Sarafadin Akin Adegboyega <Haykay2009@gmail.com>
2025-09-01 11:18:50 +0000

    update

---
+++ models/macro.sql
@@ -0,0 +1,16 @@
+-- Use the `ref` function to select from other models-
+{{ config(materialized='table') }}
+SELECT
+    o.Order_ID,
+    c.Customer_ID,
+    c.Customer_Name,
+    p.Product_ID,
+    p.Product_Name,
+    o.Quantity,
+    {{get_date_parts('Order_Date')}} as date_extract
+FROM
+    first_dataset.Orders o
+JOIN
+    {{ ref("stg_customers") }} c ON o.Customer_ID = c.Customer_ID
+JOIN
+    first_dataset.Products p ON o.Product_ID = p.Product_ID
